6f9be1a88dbcccf4b4a65c49381ae6ac44c917ff
mtmarctoni <marctonimas@protonmail.com>
2025-03-18 18:09:48 +0100

    add components and style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import svgLoader from 'vite-svg-loader';
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss(),
+      svgLoader()
+    ],
+  },
+  integrations: [vue()],
+  
+});
