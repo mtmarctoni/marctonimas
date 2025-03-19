d6d3f0b050ae78ff1a63012ac1159132b1102402
mtmarctoni <marctonimas@protonmail.com>
2025-03-19 15:00:20 +0100

    add prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import pluginAstro from "eslint-plugin-astro";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,vue,astro}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  ...pluginAstro.configs.recommended,
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      // other rules...
+    }
+  }
+];
